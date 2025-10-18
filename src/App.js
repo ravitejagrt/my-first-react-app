d9a1a69a849dd4e5b7daef6c8092aa88d8310815
ravitejagrt <raviteja.gajarla@gmail.com>
2025-10-18 14:49:58 -0500

    updated api response line

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
       <h1>Hello World!</h1>
       <p>I'm Ravi Teja. This is my first react app. I'm trying to call spring boot apis from this react app and trying to dispaly some data on this react webpage.</p>
-      <p>API Response: {message}</p>
+      <p>API Response: Endpoint /helloWord - {message}</p>
     </div>
   );
 }
